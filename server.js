65a0ddf93009da4b19e2ebe69a4217ad654f5623
Kevin <kevin.lin@leinotek.com>
2024-10-10 22:00:40 +0800

    make changes for deploment

--- server.js
+++ server.js
@@ -10,10 +10,12 @@ require("./config/passport")(passport);
 //因為 client 和 server port 不一樣，會有跨來源訪問的問題
 //所以後端需要設置 cors 來允許接受前端 react request
 const cors = require("cors");
+const path = require("path");
+const port = process.env.PORT || 8080; // process.env.PORT 是 Heroku 自動動態設定
 
 //連結到 mongoDB
 mongoose
-  .connect("mongodb://localhost:27017/mernDB")
+  .connect(process.env.MONGODB_CONNECTION)
   .then(() => {
     console.log("連結到 mongodb...");
   })
@@ -25,6 +27,8 @@ mongoose
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(express.static(path.join(__dirname, "client", "build")));
+
 //有關 api/user 的需求都由 authRoute 處理
 app.use("/api/user", authRoute);
 
@@ -37,8 +41,17 @@ app.use(
   courseRoute
 );
 
+//網站的首頁網址是 URL/....
+//這個意思是：Horkou 接收到除了上述有 api 的 routes ，且當 env 是 product 或 staging
+//的時候， horkou 就會導向到 client 的路徑，也就是 client : xx/index
+if (process.env.NODE_ENV === "producet" || process.env.NODE_ENV === "staging") {
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+
 //port 3000 是 React 預設
 //這裡用 8080
-app.listen(8080, () => {
+app.listen(port, () => {
   console.log("後端伺服器聆聽 port 8080");
 });
